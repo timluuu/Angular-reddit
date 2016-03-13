0c8382bda19118cdb05fc9595cd3a8a10869e105
Timothy Luu <timluuu@yahoo.com>
2016-03-13 01:56:01 +0000

    finished project

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -1,11 +1,5 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
-app.factory('posts', [function(){
-    var o = {
-        posts: []
-    };
-    return o;
-}]);
 
 app.config([
     '$stateProvider',
@@ -16,12 +10,22 @@ app.config([
             .state('home', {
                 url: '/home',
                 templateUrl: '/home.html',
-                controller: 'MainCtrl'
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['posts', function(posts){
+                        return posts.getAll();
+                    }]
+                }
             })
             .state('posts', {
                 url: '/posts/{id}',
                 templateUrl: '/posts.html',
-                controller: 'PostsCtrl'
+                controller: 'PostsCtrl',
+                resolve: {
+                    post: ['$stateParams', 'posts', function($stateParams, posts) {
+                        return posts.get($stateParams.id);
+                    }]
+                }
             });
             
         $urlRouterProvider.otherwise('home');
@@ -35,44 +39,90 @@ app.controller('MainCtrl', [
         $scope.posts =  posts.posts;
         $scope.addPost = function() {
             if(!$scope.title || $scope.title === '') {return; }
-            $scope.posts.push({title: $scope.title,
-                               link: $scope.link, 
-                               upvotes: 0,
-                               comments: [
-                                   {author: 'Joe', body: 'Cool post!', upvotes:0},
-                                   {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                                    ]
+            posts.create({
+                title: $scope.title,
+                link: $scope.link,
             });
             $scope.title = '';
             $scope.link = '';
         };
         
         $scope.incrementUpvotes = function(post) {
-            post.upvotes += 1;
+            posts.upvote(post);
         };
     }]);
     
 app.controller('PostsCtrl', [
     '$scope',
-    '$stateParams',
     'posts',
-    function($scope, $stateParams, posts){
-        $scope.post = posts.posts[$stateParams.id];
+    'post',
+    function($scope, posts, post){
+        $scope.post = post;
         $scope.addComment = function(){
             
             console.log("Im here");
             
             if($scope.body === '') {return; }
-            $scope.post.comments.push({
+            posts.addComment(post._id, {
                 body: $scope.body,
                 author: 'user',
-                upvotes: 0
+            }).success(function(comment) {
+                $scope.post.comments.push(comment);
             });
             $scope.body = '';
         };
-    }])
+        $scope.incrementUpvotes = function(comment) {
+            comment.upvotes += 1;
+        };
+        
+        $scope.incrementUpvotes = function(comment){
+            posts.upvoteComment(post, comment);
+        };
+    }]);
     
+app.factory('posts', ['$http', function($http){
+    var o = {
+        posts: []
+    };
     
+    o.getAll = function() {
+        return $http.get('/posts').success(function(data) {
+            angular.copy(data, o.posts);
+        });
+    };
+    
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data){ 
+            o.posts.push(data);
+        });
+    };
+    
+    o.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote').success(function(data) {
+                post.upvotes += 1;
+            });
+    };
+    
+    o.get = function(id) {
+        return $http.get('/posts/' + id).then(function(res){
+            return res.data;
+        });
+    };
+    
+    o.addComment = function(id, comment) {
+        return $http.post('/posts/' + id + '/comments', comment);
+    };
+    
+	o.upvoteComment = function(post, comment) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+			.success(function(data) {
+				comment.upvotes += 1;
+			});
+	};
+    
+    return o;
+}]);
+
     
     
     
